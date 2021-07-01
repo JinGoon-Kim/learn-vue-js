d291123c7e5fed7913426f174f1374a497afcdf5
JinGoon-Kim <raneata@gmail.com>
2021-07-01 23:22:47 +0900

    vue cli update

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  // JavaScript - 인스턴스 옵션
+  name: "a",
+  methods: {
+    addNum: function (){
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+  /* css */
+</style>
